2d6661a17e0f63edc29245d0d5ecc68b26209a1b
Evans Munene <munene.evansk@gmail.com>
2017-07-27 11:53:46 +0300

    Update: configured app with redux

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,15 @@
 import { render } from "react-dom"
 import React from "react"
+import { Provider } from "react-redux"
+
+import App from "./Containers/App"
+import store from "./Store/configureStore"
+
+// import 'semantic-ui-css/semantic.min.css';
 
 render(
-    <div>
-        <h1> React Boilerplate</h1>
-    </div>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('app')
-)
+);
