c62263ae78ec57897267356604fbb0ead303fb7a
Evans Munene <munene.evansk@gmail.com>
2017-07-27 11:53:19 +0300

    Update:  setup App

---
+++ src/app/Containers/App.js
@@ -0,0 +1,15 @@
+import React , { Component } from "react"
+import "babel-polyfill"
+
+//import screens here
+
+export default class App extends Component{
+    render(){
+        return(
+            //add screen inside the div
+            <div>
+                <h1>App</h1>
+            </div> 
+        );
+    }
+}
