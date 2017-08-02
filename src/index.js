93fa331036ecb3edf0ed7c5bcdba06fbd6ccaa1f
Brian Keyonzo <briankeyonzo@yahoo.com>
2017-08-02 23:04:00 +0300

    Added css and file loaders

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import { render } from "react-dom"
 import React from "react"
 import { Provider } from "react-redux"
 
+import './app.css'
+
 import App from "./Containers/App"
 import store from "./Store/configureStore"
-
-// import 'semantic-ui-css/semantic.min.css';
 
 render(
     <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 var webpack = require("webpack");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require("path");
 
-var DIST_DIR = path.resolve(__dirname,"dist");
+var DIST_DIR = path.resolve(__dirname,"public");
 var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
-    entry: SRC_DIR + "/app/index.js",
+    entry: SRC_DIR + "/index.js",
     output: {
-      path: DIST_DIR + "/app",
+      path: DIST_DIR + "/build",
       filename: "bundle.js",
-      publicPath: "/app/"
+      publicPath: "/build/"
     },
+    plugins:[
+      new ExtractTextPlugin('[name].css')
+    ],
     module: {
       loaders: [
         {
@@ -20,6 +24,15 @@ var config = {
           query: {
             presets: ["react","es2015","stage-2"]
           }
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg|eot|svg|ttf|woff|woff2)$/,
+            loader: "file-loader"
+        },
+        {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }
+            )
         }
       ]
     }
