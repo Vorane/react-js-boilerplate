c23a3ed843e0661426877aa576e1ef8498c55e29
Evans Munene <munene.evansk@gmail.com>
2018-01-20 11:29:42 +0300

    [Update]: linting in index js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import { render } from "react-dom"
 import React from "react"
 import { Provider } from "react-redux"
 
-import './app.css'
+import "./app.css"
 
 import App from "./Containers/App"
 import store from "./Store/configureStore"
 
 render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-    document.getElementById('app')
-);
+	<Provider store={store}>
+		<App  />
+	</Provider>,
+	document.getElementById("app")
+)
