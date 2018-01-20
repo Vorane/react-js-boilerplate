160b1ebf83958e8e15b9a4a6de31bbd2242b6e09
Evans Munene <munene.evansk@gmail.com>
2018-01-20 13:17:00 +0300

    [Update] : html webpack plugin and vendor js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 var webpack = require("webpack")
 var ExtractTextPlugin = require("extract-text-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 var path = require("path")
 
 var DIST_DIR = path.resolve(__dirname, "public")
 var SRC_DIR = path.resolve(__dirname, "src")
 
 var config = {
-	entry: SRC_DIR + "/index.js",
+	entry: {
+		vendor: ["react", "react-dom", "redux", "react-redux"],
+		app: SRC_DIR + "/index.js"
+	},
 	output: {
 		path: DIST_DIR + "/build",
-		filename: "main.js",
+		filename: "[name].js",
 		publicPath: "/build/"
 	},
-	plugins: [new ExtractTextPlugin("[name].css")],
 	module: {
 		rules: [
 			{
@@ -44,7 +47,17 @@ var config = {
 				use: "json-loader"
 			}
 		]
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Output Management"
+		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: "vendor", // Specify the common bundle's name.
+			// filename: "vendor.js",
+			minChunks: Infinity
+		})
+	]
 }
 
 module.exports = config
