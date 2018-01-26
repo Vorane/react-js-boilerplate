88ea5534b2fb551e7c0a65c9db5fb282abe27386
Evans Munene <munene.evansk@gmail.com>
2018-01-26 14:03:19 +0300

    [ADD] : Add browser sync

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,7 @@ const common = require("./webpack.common.js")
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
 	.BundleAnalyzerPlugin
 var DashboardPlugin = require("webpack-dashboard/plugin")
+var BrowserSyncPlugin = require("browser-sync-webpack-plugin")
 
 module.exports = merge(common, {
 	devtool: "inline-cheap-module-source-map",
@@ -27,5 +28,22 @@ module.exports = merge(common, {
 			}
 		]
 	},
-	plugins: [new BundleAnalyzerPlugin(), new DashboardPlugin()]
+	plugins: [
+		new BundleAnalyzerPlugin(),
+		new DashboardPlugin(),
+		new BrowserSyncPlugin(
+			{
+				// browse to http://localhost:3000/ during development,
+				// ./public directory is being served
+				host: "localhost",
+				port: 3000,
+				server: { baseDir: ["public"] }
+			},
+			{
+				// prevent BrowserSync from reloading the page
+				// and let Webpack Dev Server take care of this
+				reload: false
+			}
+		)
+	]
 })
